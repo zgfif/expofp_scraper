d92eca8e1e47811bc49e35bbd04da5455ae2f61d
zgfif <zihzag@gmail.com>
2025-07-29 09:35:27 +0300

    refactoring of save_data

--- save_data.py
+++ save_data.py
@@ -1,13 +1,30 @@
 import csv
 
-def create_csv(filename='output.csv'):
-    with open(filename, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        # Записываем заголовки столбцов
-        writer.writerow(['id', 'name', 'description', 'address', 'phone', 'website', 'mail'])
+
+def create_csv(filename='output.csv') -> None:
+    """
+    Creates a new CSV file with predefined column headers.
+    """
+    
+    table_column_names = ('id', 'name', 'description', 'address', 'phone', 'website', 'mail')
+    
+    try:
+        with open(filename, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            # Write column headers
+            writer.writerow(table_column_names)
+    except IOError as e:
+        print(f"Error creating file {filename}: {e}")
 
 
-def add_to_csv(data: list, filename='output.csv'):
-    with open(filename, mode='a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(data)
+def add_to_csv(data: list[str], filename='output.csv') -> None:
+    """
+    Appends a row of data to the CSV file.
+    """
+
+    try:
+        with open(filename, mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(data)
+    except IOError as e:
+        print(f"Error writing to file {filename}: {e}")
